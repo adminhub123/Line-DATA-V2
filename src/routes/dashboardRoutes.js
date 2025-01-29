643b8136566e4b4fa0a5fb36b02b774112ea6f0d
Jareansuk14 <jareansuk.auttama@gmail.com>
2025-01-29 08:26:45 +0700

    first commit

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
 const auth = require('../middleware/auth');
 
-// ต้อง login ก่อนถึงจะใช้ได้
 router.use(auth);
-
-router.get('/summary', dashboardController.getDashboardSummary);
-router.get('/history', dashboardController.getHistory);
-router.post('/history', dashboardController.createHistory);
+router.get('/', dashboardController.getDashboardData);
 
 module.exports = router;
